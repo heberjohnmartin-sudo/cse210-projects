5849b7f27e846efb38942e192f03434f93566a6a
Heber Martin <heberjohnmartin@gmail.com>
2026-04-25 23:37:13 -0600

    I changed the whole program. Now it takes user input, compares their input to ranges of values, and outputs corresponding text.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,43 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+        Console.Write("What is your grade percentage? ");
+        string stringGrade = Console.ReadLine();
+        float grade = float.Parse(stringGrade);
+
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string passFail = "";
+        if (grade >= 70)
+        {
+            passFail = "Congratulations! You have passed the class!";
+        }
+        else
+        {
+            passFail = "So close! You'll get it next time tiger!";
+        }
+
+        Console.WriteLine($"Your letter grade: {letter} \n{passFail}");
     }
 }
